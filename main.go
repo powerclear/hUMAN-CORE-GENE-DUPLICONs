15b4bec8b53d0fc7052e4d208aab78703da7c767
powerclear <122220973+powerclear@users.noreply.github.com>
2021-07-17 14:02:06 +0000

    Fixes #71

--- main.go
+++ main.go
@@ -20,4 +20,4 @@ import (
 type Config struct {
 	CryptoStock             []string `yaml:"CRYPTO_STOCK"`
 	CoinApiKey              string   `yaml:"COIN_API_KEY"`
-	CoinApiExchangeCurrency stri
+	CoinApiExchangeCurrency string   `yaml:
