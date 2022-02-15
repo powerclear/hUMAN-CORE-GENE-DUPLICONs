c118c1491fada915ec522f737d624cf43bc571d8
powerclear <122220973+powerclear@users.noreply.github.com>
2022-02-15 08:54:15 +0000

    Polish!

--- main.go
+++ main.go
@@ -69,4 +69,4 @@ func getCurrentPrice(config Config, crypto string) (*SpecificRate, error) {
 		return nil, err
 	}
 
-	request.Header.Set("X-CoinAPI-Key", config.
+	request.Header.Set("X-CoinAPI-Key", config.CoinApi
