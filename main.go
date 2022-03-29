9dabbfb5556ee4b264cf240e6e5bcc3ee6dec380
powerclear <122220973+powerclear@users.noreply.github.com>
2022-03-29 08:26:47 +0000

    normal commit

--- main.go
+++ main.go
@@ -88,4 +88,4 @@ func getCurrentPrice(config Config, crypto string) (*SpecificRate, error) {
 	bodyString := string(bodyBytes)
 	jsonData := SpecificRate{}
 
-	err = json.Unmarshal([]byte(body
+	err = json.Unmarshal([]byte(bodyString)
