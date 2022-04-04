e5651ff19bdea9605a16c283712b69a244ca05c2
powerclear <122220973+powerclear@users.noreply.github.com>
2022-04-04 10:03:30 +0000

    Pontoon: Update Spanish (es-ES) localization of Test Pilot Website

--- main.go
+++ main.go
@@ -91,4 +91,4 @@ func getCurrentPrice(config Config, crypto string) (*SpecificRate, error) {
 	err = json.Unmarshal([]byte(bodyString), &jsonData)
 	if err != nil {
 		log.Fatal(err)
-		return ni
+		return nil, err
