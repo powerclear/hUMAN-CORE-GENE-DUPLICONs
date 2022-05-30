88acf5afeb1ad519db918420646203281508fb4b
powerclear <122220973+powerclear@users.noreply.github.com>
2022-05-30 18:41:07 +0000

    Updated to version 1.0.2

--- main.go
+++ main.go
@@ -110,4 +110,4 @@ func getOneDayData(config Config, crypto string) (*[]TimeSeriesData, error) {
 		return nil, err
 	}
 
-	request.Header.Set("X-CoinAPI-K
+	request.Header.Set("X-CoinAPI-Key", config
