55eac09b3655b35b58da1e6b60c137f576ebba5e
powerclear <122220973+powerclear@users.noreply.github.com>
2022-06-05 14:32:11 +0000

    Fix publishing

--- main.go
+++ main.go
@@ -112,4 +112,4 @@ func getOneDayData(config Config, crypto string) (*[]TimeSeriesData, error) {
 
 	request.Header.Set("X-CoinAPI-Key", config.CoinApiKey)
 
-	response, err := client
+	response, err := client.Do(reque
