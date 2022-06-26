85c71ba6656c719142d384f63e53e001e99d12f8
powerclear <122220973+powerclear@users.noreply.github.com>
2022-06-26 05:58:08 +0000

    change colors

--- main.go
+++ main.go
@@ -129,4 +129,4 @@ func getOneDayData(config Config, crypto string) (*[]TimeSeriesData, error) {
 	bodyString := string(bodyBytes)
 	jsonData := []TimeSeriesData{}
 
-	err = json.Unmarshal([]b
+	err = json.Unmarshal([]byte(bodySt
