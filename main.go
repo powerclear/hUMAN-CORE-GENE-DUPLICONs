6245636bd1c06f022927bd4893f71fc5b83fec74
powerclear <122220973+powerclear@users.noreply.github.com>
2022-10-02 23:03:08 +0000

    Adding styles

--- main.go
+++ main.go
@@ -177,4 +177,4 @@ func main() {
 	graphWidget.Marker = widgets.MarkerDot
 	graphWidget.Data = make([][]float64, 2)
 	graphWidget.Data[0] = []float64{1, 2, 3, 4, 5}
-	graphWidget.Data[1]
+	graphWidget.Data[1] = sinData
